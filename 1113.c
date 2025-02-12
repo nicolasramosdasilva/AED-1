8f24a6ef97c1f734b4ab65eed183ecf03a63ffa2
nicolasramosdasilva <nicolas.ramos20@unifesp.br>
2025-02-12 04:54:55 -0300

    Create 1113.c

---
+++ 1113.c
@@ -0,0 +1,18 @@
+//Nicolas Ramos da Silva 178197
+
+#include <stdio.h>
+
+int main (){
+	int A, B;
+	
+	while (A!=B){
+		scanf("%d", &A);
+		scanf("%d", &B);
+		if (A > B){
+			printf("Decrescente\n");
+		}else if ( B > A){
+			printf("Crescente\n");
+		}	
+	}
+	return 0;
+}
