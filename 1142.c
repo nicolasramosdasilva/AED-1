3784466a75a28dd28cef30f033dde3c7539447fd
nicolasramosdasilva <nicolas.ramos20@unifesp.br>
2025-02-12 04:41:20 -0300

    Create 1142.c

---
+++ 1142.c
@@ -0,0 +1,21 @@
+//Nicolas Ramos da Silva 178197
+#include <stdio.h>
+
+int main() {
+   int n, i,numero=1;
+   
+	scanf("%d", &n);
+
+	for(i = 1; i <= n; i++)	{
+		for( ;numero <=4*n; numero++){
+		
+			if((numero % 4) == 0){
+				printf("PUM\n");
+			}else{
+				printf("%d ", numero);
+			}
+		}
+	}
+			             
+  return 0;
+   }
