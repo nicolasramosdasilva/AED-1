505f8debae85d7f8add254ffd621c3242be78a7a
nicolasramosdasilva <nicolas.ramos20@unifesp.br>
2025-02-12 04:56:20 -0300

    Create 2936.c

---
+++ 2936.c
@@ -0,0 +1,24 @@
+//Nicolas Ramos da Silva 178197
+#include <stdio.h>
+
+#define true 1
+#define false 0
+
+int main(int argc, char **argv)
+{
+
+    int vet[5] = { 300, 1500, 600, 1000, 150 };
+    int n, ans, i;
+
+    ans = 0;
+    for (i = 0; i < 5; ++i)
+        scanf("%d", &n), ans += n * vet[i];
+
+    ans += 225;
+
+    printf("%d\n", ans);
+
+
+    return 0;
+
+}
